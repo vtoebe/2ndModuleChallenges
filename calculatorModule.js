f3e5b2420c81b1bbf1c9111bde95cb5fe5b6c698
Vitória Toebe <vitoriatoebe@gmail.com>
2021-10-08 18:22:30 -0300

    calculatorModule
    Calculator using Module Pattern

---
+++ calculatorModule.js
@@ -0,0 +1,42 @@
+let calculatorModule = (() => {
+    let expression = []
+    let map = new Map();
+
+    const operators = {
+        '+': (n1, n2) => +n1 + +n2,
+        '-': (n1, n2) => +n1 - +n2,
+        '*': (n1, n2) => +n1 * +n2,
+        '/': (n1, n2) => +n1 / +n2
+    }
+
+    const getOperator = arr => arr.filter(item => ["+", "-", "*", "/"].includes(item))
+
+    const saveOperations = (arr, result) => map.set(`${arr.join('')}`, result)
+
+
+    const enter = expressionValue => {
+        expression.push(expressionValue)
+        console.log(expression)
+    }
+
+    const equals = () => {
+        let arr = expression;
+
+        const result = operators[getOperator(arr)](arr[0], arr[2])
+        saveOperations(arr, result)
+        expression = []
+        return result;
+
+    }
+
+    const list = () => console.log(map);
+
+    const reset = () => map.clear();
+
+    return {
+        enter,
+        equals,
+        list,
+        reset
+    }
+})()
