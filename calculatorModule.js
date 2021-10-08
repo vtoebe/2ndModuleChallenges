0b6e562289be2be1ed592102e2cfee30368968d9
Vitória Toebe <vitoriatoebe@gmail.com>
2021-10-08 18:30:34 -0300

    cleaning

--- calculatorModule.js
+++ calculatorModule.js
@@ -10,27 +10,21 @@ let calculatorModule = (() => {
     }
 
     const getOperator = arr => arr.filter(item => ["+", "-", "*", "/"].includes(item))
-
     const saveOperations = (arr, result) => map.set(`${arr.join('')}`, result)
-
 
     const enter = expressionValue => {
         expression.push(expressionValue)
-        console.log(expression)
     }
 
     const equals = () => {
         let arr = expression;
-
         const result = operators[getOperator(arr)](arr[0], arr[2])
         saveOperations(arr, result)
         expression = []
         return result;
-
     }
 
     const list = () => console.log(map);
-
     const reset = () => map.clear();
 
     return {
