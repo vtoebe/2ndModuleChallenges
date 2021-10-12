b58729aea8608b1ee9e953a3aefeecea183abe2e
Vitória Toebe <vitoriatoebe@gmail.com>
2021-10-12 01:32:00 -0300

    Matrix | Factoring?

--- theMatrix.js
+++ theMatrix.js
@@ -1,25 +1,12 @@
-const people = [{
-        name: "Angelina Jolie",
-        age: 80
-    },
-    {
-        name: "Eric Jones",
-        age: 2
-    },
-    {
-        name: "Paris Hilton",
-        age: 5
-    },
-    {
-        name: "Kayne West",
-        age: 16
-    },
-    {
-        name: "Bob Ziroll",
-        age: 100
-    }
-]
+const people = [
+    {name: "Angelina Jolie", age: 80},
+    {name: "Eric Jones", age: 2},
+    {name: "Paris Hilton", age: 5},
+    {name: "Kayne West", age: 16},
+    {name: "Bob Ziroll", age: 100}
+];
 
-const whoCanGoToTheMatrix = people.map(people => people.age >= 18 ? `${people.name} can go to The Matrix` : `${people.name} is under age!`)
+const ageCheck = element => (element.age >= 18 ? `${element.name} can go to The Matrix` : `${element.name} is under age!`)
+const whoCanGo = arr => arr.map(element => ageCheck(element))
 
-console.log(whoCanGoToTheMatrix)
+console.log(whoCanGo(people))
